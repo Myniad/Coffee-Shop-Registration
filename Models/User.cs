534c755fa960a891bd91771d37eb43f92a9c2c6f
Junyoung Kim <jkim5929@gmail.com>
2023-02-10 13:34:40 -0500

    Really actually fix password leakage, and added some cool stuff from lecture

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,8 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public bool Lactose { get; set; }
+        public string Flavor { get; set; }
 
     }
 }
